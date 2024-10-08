3d1cde07773241cefe8db68f078f063041bba2a1
Holly-Wigmore <165459716+Holly-Wigmore@users.noreply.github.com>
2024-10-08 17:32:02 +1300

    added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Forecast</h1>
-      <p>
-        This project is open-sourced on{" "}
-        <a
-          href="https://github.com/Holly-Wigmore/react-weather-project"
-          rel="noreferrer"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather Forecast</h1>
+        <p>
+          This project was created by{" "}
+          <a
+            href="https://hollyberrywigmore.com/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Holly Wigmore
+          </a>{" "}
+          is open-sourced on{" "}
+          <a
+            href="https://github.com/Holly-Wigmore/react-weather-project"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
