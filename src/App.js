a42ec0756df0d81bbce5081389fe343cbdf52446
Holly-Wigmore <165459716+Holly-Wigmore@users.noreply.github.com>
2024-10-08 15:33:30 +1300

    Added a link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
+      <p>
+        This project is open-sourced on{" "}
+        <a
+          href="https://github.com/Holly-Wigmore/react-weather-project"
+          rel="noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
